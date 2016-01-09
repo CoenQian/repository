8b61f0cc21251577662a9fcc2592d77ea2e8bd1d
JiongBull <jiongbull717@gmail.com>
2016-01-09 14:30:54 +0800

    修改CopyRight

--- app/src/main/java/com/jiongbull/tutorial/app/RootApp.java
+++ app/src/main/java/com/jiongbull/tutorial/app/RootApp.java
@@ -1,28 +1,29 @@
 /*
- * -----------------------------------------------------------------
- * Copyright (C) 2015, by Peanut Run, Shenzhen, All rights reserved.
- * -----------------------------------------------------------------
+ * Copyright JiongBull 2016
  *
- * File: RootApp
- * Author: JiongBull
- * Version: 1.0
- * Create: 2015/7/10 0010
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Changes (from 2015/7/10 0010)
- * -----------------------------------------------------------------
- * 2015/7/10 0010 : 创建 RootApp (JiongBull);
- * -----------------------------------------------------------------
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package com.jiongbull.tutorial.app;
 
-import android.app.Application;
-import android.content.Context;
+package com.jiongbull.tutorial.app;
 
 import com.facebook.stetho.Stetho;
 import com.jiongbull.tutorial.R;
 import com.jiongbull.tutorial.util.ThemeUtils;
 import com.orhanobut.logger.LogLevel;
 import com.orhanobut.logger.Logger;
+
+import android.app.Application;
+import android.content.Context;
 
 /**
  * Root application.
